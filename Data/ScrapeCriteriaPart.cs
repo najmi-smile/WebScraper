574df87640991816f0e71eac56e3557fa2c746ce
Tahir Raza <tahir.raza@accesspay.com>
2018-06-05 11:50:58 +0100

    created a web scraper for boston cars catagories

---
+++ Data/ScrapeCriteriaPart.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WebScrapper.Data
+{
+    internal class ScrapeCriteriaPart
+    {
+        public string Regex { get; set; }
+        public RegexOptions RegexOption { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using WebScrapper.Builders;
+using WebScrapper.Data;
+using WebScrapper.Workers;
+using static System.Console;
+
+namespace WebScrapper
+{
+    class Program
+    {
+        private const string Method = "search";
+        static void Main(string[] args)
+        {
+            try
+            {
+                Write("Please enter the city you like to be scraped:\t");
+                var craiglistCity = ReadLine() ?? string.Empty;
+
+                Write("Please enter the CraigList catagory:\t");
+                var craiglistCatagory = ReadLine() ?? string.Empty;
+
+                using (WebClient client = new WebClient())
+                {
+                    string content = client.DownloadString($"http://{craiglistCity.Replace(" ", string.Empty)}.craigslist.org/{Method}/{craiglistCatagory}");
+
+                    ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
+                        .WithData(content)
+                        .WithRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title-hdrlnk\"">(.*?)</a>")
+                        .WithPart(new ScrapeCriteriaPartBuilder()
+                            .WithRegex(@">(.*?)</a>")
+                            .WithRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        .WithPart(new ScrapeCriteriaPartBuilder()
+                            .WithRegex(@">(.*?)</a>")
+                            .WithRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        .Build();
+
+                    Scraper scraper = new Scraper();
+                    List<string> scrapedItems = scraper.Scrape(scrapeCriteria);
+
+                    if (scrapedItems.Any())
+                    {
+                        foreach (var item in scrapedItems) WriteLine();
+                    }
+                    else
+                    {
+                        WriteLine("There are no matches found .....");
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                WriteLine(ex);
+            }
+        }
+    }
+}
